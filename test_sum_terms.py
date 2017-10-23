f82762c82051e4b5b3e66f80887bcabb62106561
Kinley Ramson <kinleyramson@gmail.com>
2017-10-23 09:07:45 -0700

    made correct tests

--- test_sum_terms.py
+++ test_sum_terms.py
@@ -1,9 +1,23 @@
 """Test for sum_series."""
 
-from sum_terms import series_sum
-import Test
+
+import pytest
 
 
-Test.assert_equals(series_sum(1), "1.00")
-Test.assert_equals(series_sum(2), "1.25")
-Test.assert_equals(series_sum(3), "1.39")
+TEST_NUMS = [
+    (1, '1.00'),
+    (2, '1.25'),
+    (3, '1.39'),
+    # My tests below
+    (5, '1.57'),
+    (125, '2.65'),
+    (5000, '3.88'),
+    (8, '1.73')
+]
+
+
+@pytest.mark.parametrize('num, result', TEST_NUMS)
+def test_sum_terms(num, result):
+    """Test for summing the first nth term of a series."""
+    from sum_terms import series_sum
+    assert series_sum(num) == result
